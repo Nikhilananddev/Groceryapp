e8356751a1f137faff9cd43d511d4338b82d7f51
Nikhil anand <Nikhilanand1672000@gmail.com>
2021-07-17 23:56:20 +0530

    error solved

--- groceryapp/Navigation/Stacknavigation.js
+++ groceryapp/Navigation/Stacknavigation.js
@@ -1,18 +1,26 @@
 import React, {Component} from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import DrawerNaviagtion from './DrawerNaviagtion';
+import SplashScreen from '../Screens/SplashScreen';
 
 const Stack = createStackNavigator();
 export class Stacknavigation extends Component {
   render() {
     return (
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="SplashScreen">
         <Stack.Screen
           options={{
             headerShown: false,
           }}
           name="Home"
           component={DrawerNaviagtion}
+        />
+        <Stack.Screen
+          options={{
+            headerShown: false,
+          }}
+          name="SplashScreen"
+          component={SplashScreen}
         />
       </Stack.Navigator>
     );
--- groceryapp/Screens/SplashScreen.js
+++ groceryapp/Screens/SplashScreen.js
@@ -1,27 +1,43 @@
-import React from 'react';
-import {StatusBar, View, Text, ActivityIndicator} from 'react-native';
+import React, {Component} from 'react';
+import {
+  StatusBar,
+  View,
+  Text,
+  ActivityIndicator,
+  StyleSheet,
+} from 'react-native';
 import logo from '../Assets/OK_splash_screen.mp4';
 import Video from 'react-native-video';
 
 export class SplashScreen extends Component {
   constructor(props) {
     super(props);
-    // setTimeout(() => {
-    //   this.props.navigation.navigate('Home');
-    // }, 500);
+    setTimeout(() => {
+      this.props.navigation.replace('Home');
+    }, 4000);
   }
   rende;
   render() {
     return (
-      // <Video
-      //   source={require('../Assets/OK_splash_screen.mp4')}
-      //   style={{flex: 1}}
-      // />
-      <View>
-        <Text>Helllo</Text>
-      </View>
+      <Video
+        source={require('../Assets/OK_splash_screen.mp4')}
+        style={styles.backgroundVideo}
+      />
+      // <View>
+      //   <Text>Helllo</Text>
+      // </View>
     );
   }
 }
 
 export default SplashScreen;
+const styles = StyleSheet.create({
+  backgroundVideo: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    backgroundColor: '#FFF',
+  },
+});
